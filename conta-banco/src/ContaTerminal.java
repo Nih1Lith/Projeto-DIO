3ce78c421d993dc1446466f6c4a7c73c989dd912
Gabriel Oliveira <gbrlplus@gmail.com>
2024-05-24 19:18:50 -0300

    Conta Bancária

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o número da conta: ");
+        int num = scanner.nextInt();
+
+        System.out.println("Digite sua agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu saldo: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.printf("Olá, " +nome+ " obrigado por criar sua conta, sua agência é " +agencia+ ", conta" + num + "e seu saldo é R$" +saldo+ " já está disponível para saque.");
     }
 }
