320e410eda041d68952fc016c9727b3f271c566b
Gabriel Oliveira <gbrlplus@gmail.com>
2024-05-25 17:49:50 -0300

    Conta Bancária

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,20 +1,32 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class ContaTerminal {
+    //Formatação de número decimal.
+    private static final DecimalFormat decimal = new DecimalFormat("0.00");
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite o número da conta: ");
-        int num = scanner.nextInt();
+        //Criando uma condição para resolver a mensagem "Resource leak: 'scanner' is never closed".
+        try{
+            System.out.println("Digite o número da conta: ");
+            int num = scanner.nextInt();
 
-        System.out.println("Digite sua agência: ");
-        String agencia = scanner.next();
+            //Esse scanner resolve o problema de pular o input para agencia. 
+            scanner.nextLine();
+    
+            System.out.println("Digite sua agência: ");
+            String agencia = scanner.nextLine();
+    
+            System.out.println("Digite seu nome: ");
+            String nome = scanner.nextLine();
+    
+            System.out.println("Digite seu saldo: ");
+            double saldo = scanner.nextDouble();
+    
+            System.out.printf("Olá " +nome+ ", obrigado por criar sua conta, sua agência é " +agencia+ ", conta " +num+ " e seu saldo de R$" +decimal.format(saldo)+ " já está disponível para saque.");}
 
-        System.out.println("Digite seu nome: ");
-        String nome = scanner.next();
-
-        System.out.println("Digite seu saldo: ");
-        double saldo = scanner.nextDouble();
-
-        System.out.printf("Olá, " +nome+ " obrigado por criar sua conta, sua agência é " +agencia+ ", conta" + num + "e seu saldo é R$" +saldo+ " já está disponível para saque.");
+            finally{
+                scanner.close();
+            }
     }
 }
